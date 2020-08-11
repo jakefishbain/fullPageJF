ada3dbd2006f8e50b54ba7247f3c257c0d5adfaf
Jake Fishbain <jakefishbain@gmail.com>
2020-08-11 15:04:14 -0500

    initial_commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
