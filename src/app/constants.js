df1e0702130d775ad8abc0d6c9cfc8e9bd111ac3
Jake Fishbain <jakefishbain@gmail.com>
2020-08-25 10:23:54 -0500

    constants.js - skills and summary

---
+++ src/app/constants.js
@@ -0,0 +1,29 @@
+export const summary = `Dedicated developer, teacher, and candy-lover, I am a motivated 'edu-coder' striving to help others through knowledge and compassion. I approach a challenge with an open mind, breaking down the issue into its individual parts and tackling each one systematically.
+
+I pride myself in being able to connect the dots, implement a plan, and share my learnings.
+
+Having taught special education across various settings, I believe communication holds together the framework of any successful partnership. The same is true in programming, regardless of the language.
+
+I am a capable developer with a creative touch. Merging my love for learning, teaching and technology, the possibilities are endless!`
+
+export const skills =[
+  'Agile Methodologies/Scrum',
+  'Pair Programming Principles',
+  'JavaScript',
+  'Angular',
+  'Node',
+  'Express',
+  'TypeScript',
+  'ReactJS',
+  'Ruby/Ruby On Rails',
+  'GraphQL',
+  'HTML/CSS',
+  'git',
+  'jQuery',
+  'Object Oriented Programming (OOP)',
+  'TDD Principals/Testing Languages',
+  'lodash',
+  'ramda',
+  'SQL/Relational Databases (PostgreSQL, MYSQL)',
+  'Functional Programming',
+]
